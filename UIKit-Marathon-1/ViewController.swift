775dca9e9037a0e456a19ac89a6e09bcf0dec557
masnumberone <m.myxortov2011@gmail.com>
2023-05-03 11:06:47 +0300

    done technical task

--- UIKit-Marathon-1/ViewController.swift
+++ UIKit-Marathon-1/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var squareView = UIView()
+    var squareViewLayer = CAGradientLayer()
+    let squareSize = CGSize(width: 100, height: 100)
+    let gradientColors = [#colorLiteral(red: 0.984562099, green: 0.02928770892, blue: 0.2500837147, alpha: 1), #colorLiteral(red: 0.08912231773, green: 0.4647620916, blue: 0.6973915696, alpha: 1)]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(squareView)
+        
+        squareView.translatesAutoresizingMaskIntoConstraints = false
+        squareView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        squareView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100).isActive = true
+        squareView.widthAnchor.constraint(equalToConstant: squareSize.width).isActive = true
+        squareView.heightAnchor.constraint(equalToConstant: squareSize.height).isActive = true
+        
+        squareView.layer.shadowPath = UIBezierPath(rect: squareView.bounds).cgPath
+        squareView.layer.addSublayer(squareViewLayer)
     }
+        
+    override func viewDidLayoutSubviews() {
+        squareViewLayer.frame = squareView.bounds
 
+        squareViewLayer.colors = gradientColors.map({ $0.cgColor })
+        squareViewLayer.startPoint = CGPoint(x: 1, y: 0)
+        squareViewLayer.endPoint = CGPoint(x: 0, y: 1)
+        squareViewLayer.cornerRadius = 16
 
+        squareViewLayer.shadowColor = UIColor.black.cgColor
+        squareViewLayer.shadowOpacity = 0.2
+        squareViewLayer.shadowOffset = CGSize(width: -8, height: 8)
+    }
 }
-
